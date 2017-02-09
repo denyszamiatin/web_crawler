40f4a42744e7aa861454f7a740f6c79745adeeb7
Nativeborn <nativeborncitizen@gmail.com>
2017-02-09 19:23:41 +0200

    Code-review #2

--- main.py
+++ main.py
@@ -28,40 +28,37 @@ def request_url(url):
     return html
 
 
-def gues_code_with_chardet(html):
+def guess_encoding_with_chardet(html):
     '''
     use if re fails to guess coding type
     '''
     data = chardet.detect(html)
-    code = data['encoding']
-    return code
+    return data['encoding']
 
 
-def guess_code(html):
+def guess_encoding(html):
     '''
     take coding type name
     '''
     html = str(html)
-    coding = re.search(r'charset[^>, ]*', html)
+    coding = re.search(r'charset[^>, ]*', html) # TODO: charset\s*=\s*['"][\\/]{0,3}(.+)['"]
     coding = coding.group(0).lower()[8:]
-    code = ''
+    encoding = ''
     for i in coding:
         if i not in ['/', '\\', '=', ' ', '"', "'"]:
-            code += i
-    return code
+            encoding += i
+    return encoding
 
 
-def decode_html(code, html):
+def decode_html(encoding, html):
     '''
     use coding name to decode site
     '''
     try:
-        decoded = html.decode(code)
-        return decoded
-    except:
-        code = gues_code_with_chardet(html)
-        decoded = html.decode(code)
-        return decoded
+        return html.decode(encoding)
+    except UnicodeDecodeError:
+        encoding = guess_encoding_with_chardet(html)
+        return html.decode(encoding)
 
 
 #validate_url('www.google.com')
@@ -72,6 +69,6 @@ url = validate_url('http://rozetka.com.ua/')            #utf-8
 #url = validate_url('http://newspaper.jfdaily.com/')    #gb2312
 #url = validate_url('http://news.livedoor.com/')        #euc-jp
 html = request_url(url)
-code = guess_code(html)
+code = guess_encoding(html)
 decoded_site = decode_html(code, html)
 print(code)
