606c2cdb7d0db56eab22dde25d89afbc6f039990
AnatoliySt20 <strikun.a.p@gmail.com>
2017-03-16 17:28:17 +0200

    rozetka + sql

--- main2.py
+++ main2.py
@@ -6,20 +6,23 @@ from bs4 import BeautifulSoup
 import collections
 import threading
 import queue
+import sqlite3
 from main import Crawler
 
-class Crawler2(Crawler):
+class Crawler_Rozetka(Crawler):
     def make_price_list(self,decoded_site, parent, price='price', name='name'):
         """used on every page"""
-        soup = BeautifulSoup(decoded_site, 'html.parser')
-        for item in soup.findAll("div", {"class": 'content'}):
-            text = item.find("script", {"type": "text/javascript"}).text.strip()
-            title_ = re.search(r'title: [^\,]*', text)
+        info_dict=re.search(r'RozetkaStickyGoods_class.prototype.options.goods[^<]*',decoded_site)
+
+        if info_dict:
+            info_dict=info_dict.group(0).lower().strip()
+            title_ = re.search(r'title: [^\,]*', info_dict)
             if title_:
-                title_ = title_.group(0).lower()[7:].strip()
-            price_ = re.search(r'price: [^\,]*', text)
+                title_ = title_.group(0).lower()[8:].strip()
+            price_ = re.search(r'price: [^\,]*', info_dict)
             if price_:
-                price_ = price_.group(0).lower()[17:].strip()
+                price_ = price_.group(0).lower()[15:].strip()
+                price_=int(''.join(price_.split(' '))[1:-1])
             with self.lock:
                 self.price_list[title_].append(price_)
             print(title_,price_)
@@ -41,13 +44,37 @@ class Crawler2(Crawler):
                 new_links.add(url)
 
 
-crawler=Crawler2()
+class Storage:
+    def __init__(self,other=None):
+        if not other:
+            other = 'prices.sqlite'
+            self.connection = sqlite3.connect(other)
+            self.cursor = self.connection.cursor()
+            self.cursor.executescript('''CREATE TABLE Rozetka (
+                                         name  TEXT NOT NULL PRIMARY KEY  UNIQUE,
+                                         price    INTEGER );
+                                      ''')
+        else:
+            self.connection = sqlite3.connect(other)
+            self.cursor = self.connection.cursor()
+
+    def update_sql(self,name,price):
+        try:
+            self.cursor.execute(''' INSERT INTO Rozetka  (name, price) VALUES (?,?)''',(name,int(price),))
+            self.connection.commit()
+            print(name,price,'added')
+        except sqlite3.IntegrityError:
+            print (name,price,' cant be entered, already in db')
+
+
+crawler=Crawler_Rozetka()
 
 url = crawler.validate_url('http://rozetka.com.ua/mobile-phones/apple/c80003/v069/')
 html = crawler.request_url(url)
 code = crawler.guess_encoding(html)
 decoded_site = crawler.decode_html(code, html)
 crawler.make_price_list(decoded_site,None)
+print('hi')
 print (len(crawler.price_list),'price list length')
 
 
@@ -55,6 +82,12 @@ level_1_depth=crawler.find_sub_links({url}, 'http://rozetka.com.ua/')
 print ('level_1_depth links ', len(level_1_depth))
 crawler.multi_update(level_1_depth,code,None)
 print (len(crawler.price_list),'price list length')
+
+new_sql_db=Storage(None)                   #use if first time used
+#new_sql_db=Storage('prices.sqlite')       #use if second time used
+
+for name,price in crawler.price_list.items():
+    new_sql_db.update_sql(name,price[0])
 
 #level_2_depth=crawler.find_sub_links(level_1_depth,'http://rozetka.com.ua/')
 #print ('level_2_depth links', len(level_2_depth))
