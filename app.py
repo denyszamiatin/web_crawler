ba376b2cbf3d0751f5d7a615be857fff0a9513ba
gordon <dimagordon@gmail.com>
2017-03-23 17:58:47 +0200

    updated with select bar

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from rozetka_parser import Crawler
+
+# link_laptops = 'http://rozetka.com.ua/notebooks/c80004/filter/'
+# link_kids_nutrition = 'http://rozetka.com.ua/detskie-smesi/c2586302/'
+link_phones = 'http://rozetka.com.ua/mobile-phones/c80003/preset=smartfon/'
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    craw = Crawler('http://rozetka.com.ua/mobile-phones/c80003/preset=smartfon/')
+    titles = craw.db.select_titles()
+    return render_template('index.html', titles=titles)
 
 
-@app.route('/result')
+@app.route('/result', methods=['POST'])
 def result():
-    craw = Crawler('http://rozetka.com.ua/mobile-phones/apple/c80003/v069/')
-    data = craw.db.select_all()
+    select = request.form.get('producer')
+    global link_phones
+    link_phones = link_phones[:-1] + ';producer={}/'.format(select).lower()  # works only with link_phones
+    craw = Crawler(link_phones, main_page=False)
+    data = craw.db.select_all_data()
     return render_template('result.html', data=data)
 
-app.run()
+app.run(debug=True)
--- rozetka_parser.py
+++ rozetka_parser.py
@@ -7,11 +7,24 @@ import chardet
 
 
 class Crawler:
-    def __init__(self, url):
+    def __init__(self, url, main_page=True):
         self.page = self.get_page(url)
         self.soup = self.cook_soup(self.page)
-        self.db = Storage('rozetka_parse.db')
-        self.get_data()
+        self.db = Storage('rozetka_parse.db', main_page)
+        if main_page:
+            self.get_titles_box()
+        else:
+            self.get_data()
+
+    def get_titles_box(self):
+        data = self.soup.find(class_='filter-parametrs-i')
+        if data.get('param') == 'producer':
+            self.get_titles(str(data))
+
+    def get_titles(self, data):
+        titles = re.findall(r'"eventContent": "(\w+)"', data)
+        for title in titles:
+            self.db.insert_title(title)
 
     def get_page(self, url):
         d_page = self.request_url(url)
@@ -54,14 +67,15 @@ class Crawler:
         title_box = data.find('div', 'g-i-tile-i-title')
         title = title_box.text.strip()
         link = title_box.find('a').get('href')
-
         img = data.find('img').get('data_src')
         price = str(data.find('script'))
         price = re.split('\n', price)
         price = self.get_pricerawjson(price)
-        price = re.match('var pricerawjson = "%7B%22price%22%3A(\d+)', price).group(1)
-        # price = price.group(1)
-        self.db.insert(title, price, img, link)
+        try:
+            price = re.match(r'var pricerawjson = "%7B%22price%22%3A(\d+)', price).group(1)
+            self.db.insert_data(title, price, img, link)
+        except TypeError:
+            print('No items')
 
     @staticmethod
     def get_pricerawjson(data):
@@ -71,28 +85,40 @@ class Crawler:
 
 
 class Storage:
-    def __init__(self, filename):
+    def __init__(self, filename, main_page):
         self.conn = sqlite3.connect(filename)
         self.cur = self.conn.cursor()
-        self.cur.execute('''DROP TABLE IF EXISTS Rozetka''')
-        self.cur.execute('''CREATE TABLE Rozetka(
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                title TEXT NOT NULL,
-                                price TEXT NOT NULL,
-                                img   TEXT NOT NULL,
-                                link  TEXT NOT NULL)
-                        ''')
+        if main_page:
+            self.cur.execute('''DROP TABLE IF EXISTS Titles''')
+            self.cur.execute('''CREATE TABLE Titles(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    title TEXT NOT NULL)
+                            ''')
+        else:
+            self.cur.execute('''DROP TABLE IF EXISTS Rozetka''')
+            self.cur.execute('''CREATE TABLE Rozetka(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    title TEXT NOT NULL,
+                                    price TEXT NOT NULL,
+                                    img   TEXT NOT NULL,
+                                    link  TEXT NOT NULL)
+                            ''')
 
-    def insert(self, title, price, img, link):
-        try:
-            self.cur.execute(''' INSERT INTO Rozetka  (title, price, img, link)
-                                 VALUES (?,?,?,?)''', (title, price, img, link,))
-            self.conn.commit()
-        except sqlite3.IntegrityError:
-            print('DBinsertError')
+    def insert_data(self, title, price, img, link):
+        self.cur.execute(''' INSERT INTO Rozetka  (title, price, img, link)
+                             VALUES (?,?,?,?)''', (title, price, img, link,))
+        self.conn.commit()
 
-    def select_all(self):
+    def select_all_data(self):
         self.cur.execute('SELECT title, price, img, link FROM Rozetka')
+        return self.cur.fetchall()
+
+    def insert_title(self, title):
+        self.cur.execute('''INSERT INTO Titles  (title)
+                            VALUES (?)''', (title,))
+
+    def select_titles(self):
+        self.cur.execute('SELECT title FROM Titles')
         return self.cur.fetchall()
 
 
